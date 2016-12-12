079e38143d2881451b562916237797239a5260ca
HanGuang <2313303800@qq.com>
2016-12-12 08:56:28 +0800

    fix2

--- script/h.js
+++ script/h.js
@@ -25,8 +25,8 @@ $(document).ready(function() {
                 status = 1;
                 topx = -(num - 1) * winHeight;
                 obj.css('top', topx + "px");
-                $('#zhuangt li').find('span').removeClass('select');
-                $('#zhuangt li').eq(num - 1).find('span').addClass('select');
+                $('#state li').find('span').removeClass('select');
+                $('#state li').eq(num - 1).find('span').addClass('select');
 
                 setTimeout(function() {
                     status = 0;
@@ -41,8 +41,8 @@ $(document).ready(function() {
                 status = 1;
                 topx = -(num + 1) * winHeight;
                 obj.css('top', topx + "px");
-                $('#zhuangt li').find('span').removeClass('select');
-                $('#zhuangt li').eq(num + 1).find('span').addClass('select');
+                $('#state li').find('span').removeClass('select');
+                $('#state li').eq(num + 1).find('span').addClass('select');
                 setTimeout(function() {
                     status = 0;
                     obj.attr('data-num', num + 1);
@@ -54,7 +54,8 @@ $(document).ready(function() {
         }
     });
 
-    $('#zhuangt li').click(function() {
+    /*状态小圆点击事件*/
+    $('#state li').click(function() {
         var obj = $("div.sectionwrap");
         var num = $(this).index();
         var winHeight = $(window).height();
@@ -66,35 +67,71 @@ $(document).ready(function() {
         }, 300, function() {
             status = 0;
             obj.attr('data-num', num);
-            $('#zhuangt li').find('span').removeClass('select');
-            $('#zhuangt li').eq(num).find('span').addClass('select');
+            $('#state li').find('span').removeClass('select');
+            $('#state li').eq(num).find('span').addClass('select');
         });
 
         obj.css('top', topx + "px");
-        $('#zhuangt li').find('span').removeClass('select');
-        $('#zhuangt li').eq(num).find('span').addClass('select');
+        $('#state li').find('span').removeClass('select');
+        $('#state li').eq(num).find('span').addClass('select');
         setTimeout(function() {
             status = 0;
             obj.attr('data-num', num);
-            /* if (num === 3) {
-                 $('.nextbtn').find('i').attr('class', 'fa fa-angle-up');
-             } else {
-                 $('.nextbtn').find('i').attr('class', 'fa fa-angle-down');
-             };*/
+            if (num === 3) {
+                $('.nextbtn').find('i').attr('class', 'fa fa-angle-up');
+            } else {
+                $('.nextbtn').find('i').attr('class', 'fa fa-angle-down');
+            };
         }, 600);
 
     });
+
+    /*下一页按钮*/
+    $('.nextbtn').click(function() {
+        $(this).css('background', 'rgba(0,0,0,0.7)');
+        setTimeout(function() {
+            $('.nextbtn').css('background', 'none');
+        }, 100);
+        var obj = $(".sectionwrap");
+        var num = obj.attr('data-num') * 1;
+
+        if (num < 4 && status == 0) {
+            status = 1;
+            topx = topx - winHeight;
+            obj.css('top', topx + "px");
+            $('#state li').find('span').removeClass('select');
+            $('#state li').eq(num + 1).find('span').addClass('select');
+            setTimeout(function() {
+                status = 0;
+                obj.attr('data-num', num + 1);
+                if (num === 3) {
+                    $('.nextbtn').find('i').attr('class', 'fa fa-angle-up');
+                };
+            }, 600);
+        }
+        if (num == 4 && status == 0) {
+            status = 1;
+            topx = 0;
+            obj.css('top', "0px");
+            $('#state li').find('span').removeClass('select');
+            $('#state li').eq(0).find('span').addClass('select');
+            setTimeout(function() {
+                status = 0;
+                obj.attr('data-num', 0);
+                $('.nextbtn').find('i').attr('class', 'fa fa-angle-down');
+            }, 600);
+        };
+    });
     /*栅格菜单动画*/
     $(".menubtn").hover(function() {
-        $(this).find('i').eq(1).animate({ 'margin-top': '7px','margin-bottom':'7px' }, 100);
+        $(this).find('i').eq(1).animate({ 'margin-top': '7px', 'margin-bottom': '7px' }, 100);
     }, function() {
-        $(this).find('i').eq(1).animate({ 'margin-top': '5px','margin-bottom':'5px'  }, 100);
+        $(this).find('i').eq(1).animate({ 'margin-top': '5px', 'margin-bottom': '5px' }, 100);
     });
 
     /*显示隐藏菜单*/
     $(".menubtn").click(function() {
         var num = 0;
-        var listHeight = winHeight*0.14;
         var listWidth;
         if (winWidth > 992) {
             listWidth = winWidth * 0.35;
@@ -105,37 +142,30 @@ $(document).ready(function() {
         }
         $(".hidemenu")
             .animate({ width: listWidth + "px" }, 300)
-            .animate({ height: '100%'}, 300, function() {
+            .animate({ height: '100%' }, 300, function() {
                 showList();
             });
-        $('.hidemenu .maskmenu').fadeIn();
-        $("body").css({ "width": $('body').width(), "overflow": "hidden" });
+        $('.maskmenu').fadeIn();
 
-        function showList(_this) {
-            $(".hidemenu .name").fadeIn();
+        function showList() {
+            $(".name,.closemenu").fadeIn();
             setTimeout(function() {
-                $(".listmenu li a").eq(0).animate({ opacity: 1, left: "0px" });
-                $(".listmenu li a").eq(1).delay(100).animate({ opacity: 1, left: "0px" });
-                $(".listmenu li a").eq(2).delay(200).animate({ opacity: 1, left: "0px" });
-                $(".listmenu a").eq(3).delay(300).animate({ opacity: 1, left: "0px" });
-                $(".listmenu a").eq(4).delay(400).animate({ opacity: 1, left: "0px" });
-                setTimeout(function() {
-                    $(".listmenu li a").addClass('transition');
-                }, 1000);
+                $(".listmenu li a").eq(0).animate({ opacity: 1, left: 0 });
+                $(".listmenu li a").eq(1).delay(100).animate({ opacity: 1, left: 0 });
+                $(".listmenu li a").eq(2).delay(200).animate({ opacity: 1, left: 0 });
+                $(".listmenu li a").eq(3).delay(300).animate({ opacity: 1, left: 0 });
+                $(".listmenu li a").eq(4).delay(400).animate({ opacity: 1, left: 0 });
             }, 200);
         }
     });
 
+    /*绑定关闭菜单事件*/
     $(".closemenu,.maskmenu").click(function() {
-        $(".listmenu li a").removeClass('transition').css({ opacity: 0, left: "7%" });
+        $(".listmenu li a").css({ opacity: 0, left: "7%" });
         $(".closemenu,.name,.maskmenu").fadeOut();
         $(".hidemenu")
             .animate({ height: "5px" })
-            .animate({ width: 0 }, function() {
-                $("body").css({ "width": "auto", "overflow": "auto" });
-            });
+            .animate({ width: 0 });
     });
-
-
 
 });
