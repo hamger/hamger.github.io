7e3103cd4135e64d195a14efd40b2efccf02112c
Hanger <2313303800@qq.com>
2018-10-03 23:25:04 +0800

    Change theme

--- themes/next/scripts/tags/exturl.js
+++ themes/next/scripts/tags/exturl.js
@@ -4,7 +4,9 @@
 
 'use strict';
 
+/*jshint camelcase: false */
 var util = require('hexo-util');
+/*jshint camelcase: true */
 var htmlTag = util.htmlTag;
 
 var rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/;
